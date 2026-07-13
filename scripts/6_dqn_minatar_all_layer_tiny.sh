7cb039053f3b76945269a4c5b3ba56de67010916
ptekou <pako.tekou@inria.fr>
2026-07-13 10:13:25 +0000

    add: wandb configuration

--- scripts/6_dqn_minatar_all_layer_tiny.sh
+++ scripts/6_dqn_minatar_all_layer_tiny.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 COMMAND="uv run cleanrl/feature_rank/6_dqn_minatar_tiny_all_layer.py \
+    --track \
+    --wandb-project-name "growing_network_for_feature_collapse" \
+    --wandb-entity "tekoumaxencelebaron-cole-normale-sup-rieure-paris-saclay" \
     --capture-video \
     --total-timesteps 5_000_000 \
     --buffer-size 100_000 \
@@ -14,7 +17,7 @@ COMMAND="uv run cleanrl/feature_rank/6_dqn_minatar_tiny_all_layer.py \
 
 uv run python scripts/submit.py \
     --env-ids "MinAtar/Breakout-v1" \
-    --seeds 1 \
+    --seeds 1 2 3 4 5 \
     --command "$COMMAND" \
     --gres "gpu:1" \
     --time "7-00:00:00" \
--- scripts/dqn_minatar_test.sh
+++ scripts/dqn_minatar_test.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-COMMAND="uv run cleanrl/feature_rank/dqn_minatar_grow_hidden_only.py \
+COMMAND="uv run cleanrl/feature_rank/1_dqn_minatar.py \
+    --track \
+    --wandb-project-name "growing_network_for_feature_collapse" \
+    --wandb-entity "tekoumaxencelebaron-cole-normale-sup-rieure-paris-saclay" \
     --capture-video \
-    --total-timesteps 150_000 \
-    --buffer-size 20_000 \
-    --batch_size 128 \
+    --total-timesteps 5_000_000 \
+    --buffer-size 100_000 \
     --start-e 1 \
-    --end-e 0.1 \
-    --exploration-fraction 0.02 \
-    --feature-rank-n-states 1000 \
+    --end-e 0.01 \
+    --exploration-fraction 0.05 \
+    --learning-starts 10_000 \
+    --train-frequency 1 \
+    --feature-rank-n-states 2000 \
     --return-window-size 100 \
-    --learning-starts 5_000 \
-    --compute-final-feature-rank \
-    --plasticity-n-steps 100 \
-    --plasticity-n-tasks 5 \
-    --growth-adjust-steps 100"
+    --compute-final-feature-rank"
 
 uv run python scripts/submit.py \
-    --env-ids "MinAtar/Asterix-v1" \
-    --seeds 1 \
+    --env-ids "MinAtar/Breakout-v1" \
+    --seeds 1 2 3 4 5 \
     --command "$COMMAND" \
     --gres "gpu:1" \
     --time "7-00:00:00" \
     "$@"
-
-# "MinAtar/Asterix-v1" "MinAtar/Freeway-v1" "MinAtar/Seaquest-v1" "MinAtar/SpaceInvaders-v1"\
