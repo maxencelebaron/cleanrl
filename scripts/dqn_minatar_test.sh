8f1b3cdc70fcd1c2c5c1e0028ee680a9b4b5234c
ptekou <pako.tekou@inria.fr>
2026-07-14 13:43:37 +0000

    adding MinAtar to the pyproject and first test

--- scripts/dqn_minatar_test.sh
+++ scripts/dqn_minatar_test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 COMMAND="uv run cleanrl/orthogonal_feature_dqn/1_dqn_minatar.py \
-    --track \
-    --wandb-project-name "growing_network_for_feature_collapse" \
-    --wandb-entity "tekoumaxencelebaron-cole-normale-sup-rieure-paris-saclay" \
     --capture-video \
     --total-timesteps 100_000 \
     --buffer-size 10_000 \
